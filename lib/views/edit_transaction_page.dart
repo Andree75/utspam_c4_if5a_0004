6bf1c10a757bac8890f67cfff568e3694146001a
Andri <jamanbuntutan75@gmail.com>
2025-11-22 15:11:27 +0700

    commit17

--- lib/views/edit_transaction_page.dart
+++ lib/views/edit_transaction_page.dart
@@ -11,3 +11,35 @@ class EditTransactionPage extends StatefulWidget {
   @override
   State<EditTransactionPage> createState() => _EditTransactionPageState();
 }
+
+class _EditTransactionPageState extends State<EditTransactionPage> {
+  final _formKey = GlobalKey<FormState>();
+  late TextEditingController _qtyController;
+  late TextEditingController _recipeController;
+
+  // Kita tidak mengedit nama obat/harga satuan, hanya detail pembelian
+  String _selectedMethod = "Langsung";
+  int _totalPrice = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    // Isi form dengan data lama
+    _qtyController = TextEditingController(
+      text: widget.transaction.quantity.toString(),
+    );
+    _recipeController = TextEditingController(
+      text: widget.transaction.recipeNumber ?? "",
+    );
+    _selectedMethod = widget.transaction.type;
+    _totalPrice = widget.transaction.totalPrice;
+
+    _qtyController.addListener(_updateTotal);
+  }
+
+  void _updateTotal() {
+    int qty = int.tryParse(_qtyController.text) ?? 0;
+    setState(() {
+      _totalPrice = qty * widget.transaction.price;
+    });
+  }
